545e957dad5cb6306882de520d60f881ed20d3df
John Wu <jhnwu3@gmail.com>
2021-10-17 14:38:51 -0400

    $

--- linear.sh
+++ linear.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=linearDebug
 #SBATCH --partition=general
 #SBATCH --nodes=1
-#SBATCH --output=./slurm_outputs/linearDebug%j.txt
+#SBATCH --output=./linearDebug%j.txt
 #SBATCH --cpus-per-task=1
 # load all modules, build terminal code, move all outputs into output folders.
 
