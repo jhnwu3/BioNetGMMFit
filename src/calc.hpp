6f41b9a30501f932dba95bea6d72c2761d710865
John Wu <jhnwu3@gmail.com>
2021-10-29 15:15:06 -0400

    $

--- src/calc.hpp
+++ src/calc.hpp
@@ -4,4 +4,4 @@
 double calculate_cf1(const VectorXd& trueVec, const VectorXd& estVec);
 double calculate_cf2(const VectorXd& trueVec, const  VectorXd& estVec, const MatrixXd& w);
 MatrixXd customWtMat(const MatrixXd& Yt, const MatrixXd& Xt, int nMoments, int N);
-#endif _CALC_HPP_
+#endif 
