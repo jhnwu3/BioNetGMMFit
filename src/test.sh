c403365436c35d36fabade629dccc500a966c37a
John Wu <jhnwu3@gmail.com>
2022-01-02 14:21:28 -0500

    $

--- src/test.sh
+++ src/test.sh
@@ -5,6 +5,12 @@
 #SBATCH --nodes=1
 #SBATCH --output=test%j.txt
 #SBATCH --cpus-per-task=30
+
+# loads necessary modules on NCH cluster, use source load.sh
+git pull
+module load Eigen GCC/9.3.0 OpenMPI/4.0.3 Boost/1.72.0 gnuplot
+make
+
 # load all modules, build terminal code, move all outputs into output folders.
 chmod u+x sig
 ./sig
