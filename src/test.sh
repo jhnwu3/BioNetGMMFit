c3aed660d0e53ea4b26ab4e6e8d89cc330442327
John Wu <jhnwu3@gmail.com>
2022-01-11 18:10:01 -0500

    $

--- src/test.sh
+++ src/test.sh
@@ -6,6 +6,11 @@
 #SBATCH --output=test%j.txt
 #SBATCH --cpus-per-task=30
 
+# please just load this in. :l
+git pull
+module load Eigen GCC/9.3.0 OpenMPI/4.0.3 Boost/1.72.0 gnuplot
+make
+
 # load all modules, build terminal code, move all outputs into output folders.
 chmod u+x sig
 ./sig
