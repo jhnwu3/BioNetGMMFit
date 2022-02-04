48cd4643c8e42dfe0b12cc2a2eabd4fb3e99cae5
John Wu <jhnwu3@gmail.com>
2022-02-04 11:41:02 -0500

    more comments, more readme, minor shell script things

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+
+echo "Installing necessary libraries into Ubuntu System. Should an error occur, please consult the readme for specific install instructions!"
 cd /usr/local/include
 sudo ln -sf eigen3/Eigen Eigen
 sudo ln -sf eigen3/unsupported unsupported
--- src/system.hpp
+++ src/system.hpp
@@ -4,6 +4,8 @@
 #include "linear.hpp"
 #include "nonlinear.hpp"
 MatrixXd interactionMatrix(int nSpecies, const VectorXd &k);
+
+/* Nonlinear ODE System to be defined for nonlinear system. Simply refer to the comments for each term. */
 class Nonlinear_ODE
 {
     struct K rate;
@@ -13,7 +15,7 @@ public:
 
     void operator() (const State_N& c, State_N& dcdt, double t)
     {
-        dcdt[0] = -(rate.k(0) * c[0] * c[1])  // Syk
+        dcdt[0] = -(rate.k(0) * c[0] * c[1])  // Syk = dc1/dt = k1 *c1*c2 + k2*c3 + k3*c3 
             + rate.k(1) * c[2]
             + rate.k(2) * c[2];
 
