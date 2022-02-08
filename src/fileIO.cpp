cf519fa0ad97e93718cb62f16a997dcfca19d2b4
John Wu <jhnwu3@gmail.com>
2022-02-08 15:52:29 -0500

    $

--- src/fileIO.cpp
+++ src/fileIO.cpp
@@ -240,7 +240,7 @@ int readCsvPSO(int &nPart1, int &nSteps1, int &nPart2, int &nSteps2, int &useOnl
         std::stringstream ss(line); // make a string stream from the line such that you can isolate each word even further.
         string col;
         while(std::getline(ss, col, ',')){
-            if(isNumber(col)){ // only add into parameter vector if actually an int.
+            if(isNumber(col) || isDouble(col)){ // only add into parameter vector if actually an int.
                 params.push_back(std::stod(col)); 
             }
         }
