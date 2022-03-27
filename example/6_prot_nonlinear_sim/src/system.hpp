b06e17d882f1b3d6ce2c0110a03c91054859b53d
John Wu <jhnwu3@gmail.com>
2022-03-27 14:45:55 -0400

    example change

--- example/6_prot_nonlinear_sim/src/system.hpp
+++ example/6_prot_nonlinear_sim/src/system.hpp
@@ -8,36 +8,39 @@ MatrixXd interactionMatrix(int nSpecies, const VectorXd &k);
 /* Nonlinear ODE System to be defined for nonlinear system. Simply refer to the comments for each term. */
 class Nonlinear_ODE
 {
-    struct K rate;
+    // estimate vector
+    VectorXd k;
 
 public:
-    Nonlinear_ODE(struct K G) : rate(G) {}
+    Nonlinear_ODE(VectorXd G) : k(G) {}
 
     void operator() (const State_N& c, State_N& dcdt, double t)
     {
-        dcdt[0] = -(rate.k(0) * c[0] * c[1])  // Syk = dc1/dt = k1 *c1*c2 + k2*c3 + k3*c3 
-            + rate.k(1) * c[2]
-            + rate.k(2) * c[2];
+        /* WRITE YOUR SYSTEM OF DIFFERENTIAL EQUATIONS BELOW, BEWARE OF THE "()"" INDEXING FOR YOUR PARAMETERS AND "[]"" FOR YOUR PROTEINS */
 
-        dcdt[1] = -(rate.k(0) * c[0] * c[1]) // Vav
-            + rate.k(1) * c[2]
-            + rate.k(5) * c[5];
+        dcdt[0] = -(k(0) * c[0] * c[1])  // Syk = dc1/dt = k1 *c1*c2 + k2*c3 + k3*c3 
+            + k(1) * c[2]
+            + k(2) * c[2];
 
-        dcdt[2] = rate.k(0) * c[0] * c[1] // Syk-Vav
-            - rate.k(1) * c[2]
-            - rate.k(2) * c[2];
+        dcdt[1] = -(k(0) * c[0] * c[1]) // Vav
+            + k(1) * c[2]
+            + k(5) * c[5];
 
-        dcdt[3] = rate.k(2) * c[2] //pVav
-            - rate.k(3) * c[3] * c[4]
-            + rate.k(4) * c[5];
+        dcdt[2] = k(0) * c[0] * c[1] // Syk-Vav
+            - k(1) * c[2]
+            - k(2) * c[2];
 
-        dcdt[4] = -(rate.k(3) * c[3] * c[4]) // SHP1 
-            + rate.k(4) * c[5]
-            + rate.k(5) * c[5];
+        dcdt[3] = k(2) * c[2] //pVav
+            - k(3) * c[3] * c[4]
+            + k(4) * c[5];
 
-        dcdt[5] = rate.k(3) * c[3] * c[4]  // SHP1-pVav
-            - rate.k(4) * c[5]
-            - rate.k(5) * c[5];
+        dcdt[4] = -(k(3) * c[3] * c[4]) // SHP1 
+            + k(4) * c[5]
+            + k(5) * c[5];
+
+        dcdt[5] = k(3) * c[3] * c[4]  // SHP1-pVav
+            - k(4) * c[5]
+            - k(5) * c[5];
     }
 };
 #endif
