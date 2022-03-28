f74caf90bfe7fa689b93bf6b3a688421c3820f82
John Wu <jhnwu3@gmail.com>
2022-03-28 13:47:22 -0400

    minor comment changes

--- src/system.hpp
+++ src/system.hpp
@@ -18,29 +18,10 @@ public:
     {
         /* WRITE YOUR SYSTEM OF DIFFERENTIAL EQUATIONS BELOW, BEWARE OF THE "()"" INDEXING FOR YOUR PARAMETERS AND "[]"" FOR YOUR PROTEINS */
 
-        dcdt[0] = -(k(0) * c[0] * c[1])  // Syk = dc1/dt = k1 *c1*c2 + k2*c3 + k3*c3 
-            + k(1) * c[2]
-            + k(2) * c[2];
-
-        dcdt[1] = -(k(0) * c[0] * c[1]) // Vav
-            + k(1) * c[2]
-            + k(5) * c[5];
-
-        dcdt[2] = k(0) * c[0] * c[1] // Syk-Vav
-            - k(1) * c[2]
-            - k(2) * c[2];
-
-        dcdt[3] = k(2) * c[2] //pVav
-            - k(3) * c[3] * c[4]
-            + k(4) * c[5];
-
-        dcdt[4] = -(k(3) * c[3] * c[4]) // SHP1 
-            + k(4) * c[5]
-            + k(5) * c[5];
-
-        dcdt[5] = k(3) * c[3] * c[4]  // SHP1-pVav
-            - k(4) * c[5]
-            - k(5) * c[5];
+        dcdt[0] = k(0) - k(4) * c[0]; // pcd3z
+        dcdt[1] = k(1) * c[0] - k(4) * c[1]; // pslp
+        dcdt[2] = k(2) * c[1] - k(4) * c[2]; // perk
+        dcdt[3] = k(3) * c[2] - k(4) * c[3]; //ps6
     }
 };
 #endif
