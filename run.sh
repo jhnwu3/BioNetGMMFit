43661fb19ce6036f16138b3caf2d1387697943f6
John Wu <jhnwu3@gmail.com>
2022-05-16 10:55:51 -0400

    lfs

--- run.sh
+++
@@ -1,7 +0,0 @@
-cd src
-echo "Recompiling any new changes made to program!"
-make
-cd ..
-echo "Estimation has begun!"
-./CyGMM > out.txt
-echo "Estimation finished! Please look in out.txt for newly computed estimation results!"
