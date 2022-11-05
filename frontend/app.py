18c9d7bdb0471e6a896d7047b27c1ed06478685b
John Wu <jhnwu3@gmail.com>
2022-11-04 20:51:33 -0400

    $

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request, flash, redirect
+from flask import Flask, render_template, request, flash, redirect, copy_current_request_context
+from flask_executor import Executor
+from flask_socketio import SocketIO, emit
+import subprocess
+import json
+import time
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import  FileStorage
-import subprocess
+# import subprocess
 import os, shutil
 import sys
 
@@ -18,11 +23,16 @@ def deleteFilesInDirectory(folder):
          print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 app = Flask(__name__)
+app.config['EXECUTOR_TYPE'] = 'thread'
+# app.config['EXECUTOR_MAX_WORKERS'] = 2
+executor = Executor(app)
 
+socketio = SocketIO(app)
+socketio.init_app(app, cors_allowed_origins="*")
 ALLFILES = []
 INPUTS = {"config":"", "bngl":"", "timesteps":"", "truerates":"", "xData":"", "yData":[]}
 OUTPUTS = {"estimates":"", "prediction":""}
-
+RUNNING = False
 # Get current path
 path = os.getcwd()
 # file Upload
@@ -114,6 +124,9 @@ def reset():
    INPUTS['bngl'] = ""
    return redirect('/')
 
+# @copy_current_request_context
+
+
 @app.route('/run', methods = ['POST'])
 def run():
    if request.method == 'POST':
@@ -122,42 +135,85 @@ def run():
       for key, value in INPUTS.items():
          if key != 'truerates':
             if value == "" or value == []:
-               allFull = True 
-      
-      deleteFilesInDirectory(UPLOAD_FOLDER + '/X')
-      deleteFilesInDirectory(UPLOAD_FOLDER + '/Y')
-      deleteFilesInDirectory(OUTPUT_FOLDER)
-      shutil.copyfile(UPLOAD_FOLDER + '/' + INPUTS['xData'], UPLOAD_FOLDER + '/X/' + INPUTS['xData'])
-      for f in INPUTS['yData']:
-         shutil.copyfile(UPLOAD_FOLDER + '/' + f, UPLOAD_FOLDER + '/Y/' + f)
-      
+               allFull = False 
+               
       if(allFull):
-         if INPUTS['truerates'] == "":
-            INPUTS['truerates'] = 'truerates.csv'
-         subprocess.run(['./BNGMM', '-c', UPLOAD_FOLDER+ '/' + INPUTS['config'] ,
-                         '-t', UPLOAD_FOLDER+ '/' + INPUTS['timesteps'],
-                         '-m', UPLOAD_FOLDER+ '/' + INPUTS['bngl'], 
-                         '-r', UPLOAD_FOLDER+ '/' + INPUTS['truerates'],
-                         '-x', UPLOAD_FOLDER+'/X', 
-                         '-y', UPLOAD_FOLDER+'/Y',
-                         '-o', 'frontend/static/'], cwd=".")
-         
-         onlyfiles = [f for f in os.listdir(OUTPUT_FOLDER) if os.path.isfile(os.path.join(OUTPUT_FOLDER, f))]
-         for f in onlyfiles:
-            if "_estimates.png" in f: 
-               OUTPUTS['estimates'] = 'static/' + f 
-            if "_leastCostMoments.png" in f:
-               OUTPUTS['prediction'] = 'static/' + f
-         
-         return redirect('/')
-      else:
-         error = "Missing All Required Inputs!" 
-         flash(error)
-         return redirect('/')
+         deleteFilesInDirectory(UPLOAD_FOLDER + '/X')
+         deleteFilesInDirectory(UPLOAD_FOLDER + '/Y')
+         deleteFilesInDirectory(OUTPUT_FOLDER)
+         shutil.copyfile(UPLOAD_FOLDER + '/' + INPUTS['xData'], UPLOAD_FOLDER + '/X/' + INPUTS['xData'])
+         for f in INPUTS['yData']:
+            shutil.copyfile(UPLOAD_FOLDER + '/' + f, UPLOAD_FOLDER + '/Y/' + f)
+         executor.submit(runBNGMM)
+         # executor.add_default_done_callback(my_event)
+
+         # if INPUTS['truerates'] == "":
+         #    INPUTS['truerates'] = 'truerates.csv'
+         # subprocess.run(['./BNGMM', '-c', UPLOAD_FOLDER+ '/' + INPUTS['config'] ,
+         #                 '-t', UPLOAD_FOLDER+ '/' + INPUTS['timesteps'],
+         #                 '-m', UPLOAD_FOLDER+ '/' + INPUTS['bngl'], 
+         #                 '-r', UPLOAD_FOLDER+ '/' + INPUTS['truerates'],
+         #                 '-x', UPLOAD_FOLDER+'/X', 
+         #                 '-y', UPLOAD_FOLDER+'/Y',
+         #                 '-o', 'frontend/static/'], cwd=".")
+         # executor.submit(runBNGMM) 
+      # socketio.emit('processing', json.dumps({'data': 'finished processing!'}))
+      flash('Program Running! Please Refresh to See Results!')   
+      return redirect('/')
+   else:
+      error = "Missing All Required Inputs!" 
+      flash(error)
+      return redirect('/')
+
+# @socketio.on('long-running-event')
+# def my_event(future):
+#    global RUNNING
+#    print("HEY IM HERE!")
+#    # if executor.futures.done('run'):
+#    #    executor.futures.pop('run')
+#    #    onlyfiles = [f for f in os.listdir(OUTPUT_FOLDER) if os.path.isfile(os.path.join(OUTPUT_FOLDER, f))]
+#    #    for f in onlyfiles:
+#    #       if "_estimates.png" in f: 
+#    #          OUTPUTS['estimates'] = 'static/' + f 
+#    #       if "_leastCostMoments.png" in f:
+#    #          OUTPUTS['prediction'] = 'static/' + f
+#    #    socketio.emit('processing-finished', json.dumps({'data': 'finished processing!'}))
+#    # else: 
+#       socketio.emit('processing', json.dumps({'data': 'finished processing!'}))
       
+# @executor.job
+def runBNGMM():
+   global RUNNING
+   if INPUTS['truerates'] == "":
+      INPUTS['truerates'] = 'truerates.csv'
+      
+   bngmm = ['./BNGMM', '-c', UPLOAD_FOLDER+ '/' + INPUTS['config'] ,
+                     '-t', UPLOAD_FOLDER+ '/' + INPUTS['timesteps'],
+                     '-m', UPLOAD_FOLDER+ '/' + INPUTS['bngl'], 
+                     '-r', UPLOAD_FOLDER+ '/' + INPUTS['truerates'],
+                     '-x', UPLOAD_FOLDER+'/X', 
+                     '-y', UPLOAD_FOLDER+'/Y',
+                     '-o', 'frontend/static/']
+   
+   ON_POSIX = 'posix' in sys.builtin_module_names
+   # p = subprocess.Popen(bngmm, stdout=subprocess.PIPE, bufsize=1, close_fds=ON_POSIX, cwd= '.')
+   p = subprocess.run(bngmm, cwd=".")
+   # p.terminate()
+   # executor.submit(my_event)
+   # p.terminate()
+   # p.wait()
+   onlyfiles = [f for f in os.listdir(OUTPUT_FOLDER) if os.path.isfile(os.path.join(OUTPUT_FOLDER, f))]
+   for f in onlyfiles:
+      if "_estimates.png" in f: 
+         OUTPUTS['estimates'] = 'static/' + f 
+      if "_leastCostMoments.png" in f:
+         OUTPUTS['prediction'] = 'static/' + f
+      # return render_template('index.html', inputs = INPUTS, uploadedFiles = ALLFILES, outputs=OUTPUTS)
+   
 
 if __name__ == '__main__':
    SECRET_KEY = os.urandom(24)
    app.secret_key = SECRET_KEY
    port = int(os.environ.get('PORT', 5000))
-   app.run(debug=True, host='0.0.0.0', port=port) 
+   app.run(debug=True, host='0.0.0.0', port=port)
+   # socketio.run(app, host='0.0.0.0', port=port, debug=True) 
