94269ddba8c5fae16f0e1c0a069f4629bd37e33b
John Wu <jhnwu3@gmail.com>
2023-03-18 12:22:35 -0400

    $

--- l3p_confidence_intervals.sh
+++ l3p_confidence_intervals.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=4proteinlinear
 #SBATCH --partition=general
 #SBATCH --nodes=1
-#SBATCH --output=./slurm_outputs/4pro%j.txt
+#SBATCH --output=./slurm_outputs/3pro%j.txt
 #SBATCH --cpus-per-task=30
 
 ./BNGMM -m example/3_prot_linear_sim/model.bngl -x example/3_prot_linear_sim/X -y example/3_prot_linear_sim/Y -t example/3_prot_linear_sim/time_steps.csv -r example/3_prot_linear_sim/true_rates.csv -c example/3_prot_linear_sim/Config.csv
