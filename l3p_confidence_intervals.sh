75f6d121e7bfdf5c2d00772a8c8103b65d3c5d05
John Wu <jhnwu3@gmail.com>
2023-03-19 21:56:10 -0400

    $

--- l3p_confidence_intervals.sh
+++ l3p_confidence_intervals.sh
@@ -6,4 +6,5 @@
 #SBATCH --output=./slurm_outputs/3pro%j.txt
 #SBATCH --cpus-per-task=30
 
-./BNGMM -m example/3_prot_linear_sim/model.bngl -x example/3_prot_linear_sim/X -y example/3_prot_linear_sim/Y -t example/3_prot_linear_sim/time_steps.csv -r example/3_prot_linear_sim/true_rates.csv -c example/3_prot_linear_sim/Config.csv
+./BNGMM -m example/l3p_100_sim/model.bngl -x example/l3p_100_sim/X -y example/l3p_100_sim/Y -t example/l3p_100_sim/time_steps.csv -r example/l3p_100_sim/true_rates.csv -c example/l3p_100_sim/Config.csv
+# ./BNGMM -m example/3_prot_linear_sim/model.bngl -x example/3_prot_linear_sim/X -y example/3_prot_linear_sim/Y -t example/3_prot_linear_sim/time_steps.csv -r example/3_prot_linear_sim/true_rates.csv -c example/3_prot_linear_sim/Config.csv
