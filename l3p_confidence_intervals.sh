25a617684f709657cc895221b7750fd6e7055d65
John Wu <jhnwu3@gmail.com>
2023-04-19 13:50:17 -0400

    $

--- l3p_confidence_intervals.sh
+++ l3p_confidence_intervals.sh
@@ -9,4 +9,4 @@
 # ./BNGMM -m example/l3p_100_sim/model.bngl -x example/l3p_100_sim/X -y example/l3p_100_sim/Y -t example/l3p_100_sim/time_steps.csv -r example/l3p_100_sim/true_rates.csv -c example/l3p_100_sim/Config.csv
 # ./BNGMM -m example/3_prot_linear_sim/model.bngl -x example/3_prot_linear_sim/X -y example/3_prot_linear_sim/Y -t example/3_prot_linear_sim/time_steps.csv -r example/3_prot_linear_sim/true_rates.csv -c example/3_prot_linear_sim/Config.csv
 # ./BNGMM -m example/yeast/yeast.bngl -x example/yeast/X -y example/yeast/Y -t example/yeast/time_steps.csv -r example/yeast/true_rates.csv -c example/yeast/Config.csv -hr example/yeast/heldRates.csv -f example/yeast/future_times.csv -o test/
-./BNGMM -m example/6_pro_lin_sim/6_pro_lin.bngl -c example/6_pro_lin_sim/Config.csv -x example/6_pro_lin_sim/X -y example/6_pro_lin_sim/Y -t example/6_pro_lin_sim/ts.csv -r example/6_pro_lin_sim/tr.csv -o test/l6p_meansvarscovars_only/ --g
+./BNGMM -m example/6_pro_lin_sim/6_pro_lin.bngl -c example/6_pro_lin_sim/Config.csv -x example/6_pro_lin_sim/X -y example/6_pro_lin_sim/Y -t example/6_pro_lin_sim/ts.csv -r example/6_pro_lin_sim/tr.csv -o test/l6p_meansvars_only/ --g
